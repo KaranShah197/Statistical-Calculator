2d4f40098d627627f2a7c5745261ee74ed46303f
karanshah197 <karanshah197@gmail.com>
2019-12-11 23:37:39 -0500

    Feature: Casting

--- Database/sqlite_homework.py
+++ Database/sqlite_homework.py
@@ -309,3 +309,13 @@ session.query(
     func.count(distinct(Customer.town)),
     func.count(Customer.town)
 ).all()
+
+#Casting
+from sqlalchemy import cast, Date, distinct, union
+
+session.query(
+    cast(func.pi(), Integer),
+    cast(func.pi(), Numeric(10,2)),
+    cast("2010-12-01", DateTime),
+    cast("2010-12-01", Date),
+).all()

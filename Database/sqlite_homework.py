1df0f08a224eacc0768b7ea56749b0dd548ece72
karanshah197 <karanshah197@gmail.com>
2019-12-11 19:28:12 -0500

    Feature: Committing session and creating new customer table

--- Database/sqlite_homework.py
+++ Database/sqlite_homework.py
@@ -1,12 +1,9 @@
-import datetime
-
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from pprint import pprint
 
+# 1. Created engine for sqlite
 engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -14,6 +11,7 @@ session = Session()
 # this loads the sqlalchemy base class
 Base = declarative_base()
 
+# 2. Inserting Data into customer
 #Creating Customer class
 class Customer(Base):
     __tablename__ = 'customers'
@@ -24,6 +22,11 @@ class Customer(Base):
     email = Column(String(200), nullable=False)
     address = Column(String(200), nullable=False)
     town = Column(String(50), nullable=False)
+
+
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
 
 c1 = Customer(first_name = 'Toby',
               last_name = 'Miller',
@@ -47,8 +50,12 @@ print(c2)
 print(f'Firstname: {c1.first_name}, Lastname: {c1.last_name}')
 print(f'Firstname: {c2.first_name}, Lastname: {c2.last_name}')
 
-
+# 3. Adding objects into session
 session.add(c1)
 session.add(c2)
 print(f'{c1.id}, {c2.id}')
 
+#4 creating new session and commiting it
+session.add_all([c1, c2])
+session.new
+session.commit()

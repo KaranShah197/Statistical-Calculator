6a3418a7c7448a7c01b13018dd1f6caa1afd5ccd
karanshah197 <karanshah197@gmail.com>
2019-12-11 23:33:12 -0500

    Feature: having clause

--- Database/sqlite_homework.py
+++ Database/sqlite_homework.py
@@ -291,3 +291,9 @@ session.query(func.count(Customer.id)).join(Order).filter(
     Customer.last_name == 'Green',
 ).group_by(Customer.id).scalar()
 
+# find the number of customers lives in each town
+
+session.query(
+    func.count("*").label('town_count'),
+    Customer.town
+).group_by(Customer.town).having(func.count("*") > 2).all()

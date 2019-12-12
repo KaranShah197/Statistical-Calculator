29e934e21a815d64ae469fd4d35aa740563e9e45
karanshah197 <karanshah197@gmail.com>
2019-12-11 23:13:50 -0500

    Feature: Order by desc function

--- Database/sqlite_homework.py
+++ Database/sqlite_homework.py
@@ -253,3 +253,6 @@ print(session.query(Customer).limit(2).offset(2))
 #orderby
 session.query(Item).filter(Item.name.ilike("wa%")).all()
 session.query(Item).filter(Item.name.ilike("wa%")).order_by(Item.cost_price).all()
+
+from sqlalchemy import desc
+session.query(Item).filter(Item.name.ilike("wa%")).order_by(desc(Item.cost_price)).all()

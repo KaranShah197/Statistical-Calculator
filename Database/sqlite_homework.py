5eef211c4dea46e960090d996c3fc215d89339cb
karanshah197 <karanshah197@gmail.com>
2019-12-11 23:44:42 -0500

    Feature: deleting the data

--- Database/sqlite_homework.py
+++ Database/sqlite_homework.py
@@ -340,3 +340,9 @@ session.query(Item).filter(
     Item.name.ilike("W%")
 ).update({"quantity": 60}, synchronize_session='fetch')
 session.commit()
+
+#deleting the data
+i = session.query(Item).filter(Item.name == 'Monitor').one()
+i
+session.delete(i)
+session.commit()
